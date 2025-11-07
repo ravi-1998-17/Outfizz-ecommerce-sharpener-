4d94592954d9c327173c9f212873319b458835fe
Ravi Suthar <ravisuthar1711@gmail.com>
2025-11-07 13:08:07 +0530

    Loading Spinner feature added

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,54 @@ import CartModal from "./components/cart/CartModal";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CartItem from "./components/cart/Cartitem/CartItem";
+import { Spinner } from "react-bootstrap";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const savedItems = localStorage.getItem("savedItems");
+    return savedItems ? JSON.parse(savedItems) : [];
+  });
   const [showCart, setShowCart] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  //API CALL
+  //API CALL USING AXIOS & ASYNC AWAIT
   const api = "https://api.escuelajs.co/api/v1/products";
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const res = await axios.get(api);
         // console.log(res.data);
         setProducts(res.data);
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
 
+  //Adding items to the local storage
+  useEffect(() => {
+    localStorage.setItem("savedItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
   //Adding Items to cart
   const addToCart = (product) => {
-    console.log(product);
-    setCartItems((prev) => [...prev, { ...product, qty: 1 }]);
+    setCartItems((prevItems) => {
+      const existingItem = prevItems.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        return prevItems.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      } else {
+        return [...prevItems, { ...product, qty: 1 }];
+      }
+    });
+
     setShowCart(true);
   };
 
@@ -45,7 +68,6 @@ function App() {
   const handlePurchase = () => {
     alert("Thanks for shopping!");
     setCartItems([]);
-    // setShowCart(false);
   };
 
   return (
@@ -55,8 +77,21 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route
           path="/store"
-          element={<Store productsData={products} addToCart={addToCart} />}
+          element={
+            loading ? (
+              <div
+                className="d-flex justify-content-center align-items-center"
+                style={{ height: "80vh" }}
+              >
+                <Spinner animation="border" role="status" variant="dark" />
+                <span className="ms-2 fw-semibold">Loading products...</span>
+              </div>
+            ) : (
+              <Store productsData={products} addToCart={addToCart} />
+            )
+          }
         />
+
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
