dca274eda3fef4964684674799059e8f4d87a4f6
Ravi Suthar <ravisuthar1711@gmail.com>
2025-11-09 14:18:36 +0530

    Netlify stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import CartModal from "./components/cart/CartModal";
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { Spinner } from "react-bootstrap";
-import Footer from "./components/Layout/temp_footer";
+import Footer from "./components/Layout/Footer";
 import Contact from "./pages/Contact/Contact";
 import ProductDetails from "./components/shop/ProductDetails";
 import LoginPage from "./pages/LoginPage/LoginPage";
