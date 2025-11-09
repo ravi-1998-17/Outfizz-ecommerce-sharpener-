7a08a1621cc01ddd51b9679e4bd4f0cf6e36159d
Ravi Suthar <ravisuthar1711@gmail.com>
2025-11-09 14:49:59 +0530

    Netlify stuffs

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Col, Row, Image } from "react-bootstrap";
-import AuthenticationForm from "@/components/authentication/AuthenticationForm";
+import AuthenticationForm from "@/components/Authentication/AuthenticationForm";
 import loginPageImg from "@/assets/loginPageImg.jpg";
-import classes from "@/pages/LoginPage/LoginPage.module.css"
 
 const LoginPage = () => {
   return (
